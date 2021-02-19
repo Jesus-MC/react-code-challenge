d7565129bb4a7093b33c06af525e7707ee87af1d
Jesus-MC <jesusmontalvocanche@gmail.com>
2021-02-19 14:09:30 -0600

    Updates

---
+++ precache-manifest.3d220b08b7b214df8acedae80c85b041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf4676f45862ffb9c7dd16d3bf946cb",
+    "url": "/react-code-challenge/index.html"
+  },
+  {
+    "revision": "b4bf61d4ac8203c8972a",
+    "url": "/react-code-challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f8474c11c52140d44b6c",
+    "url": "/react-code-challenge/static/css/main.1d3b5854.chunk.css"
+  },
+  {
+    "revision": "b4bf61d4ac8203c8972a",
+    "url": "/react-code-challenge/static/js/2.3f8b3442.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-code-challenge/static/js/2.3f8b3442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8474c11c52140d44b6c",
+    "url": "/react-code-challenge/static/js/main.16e2b0fb.chunk.js"
+  },
+  {
+    "revision": "996a1e3f144781fd3e60",
+    "url": "/react-code-challenge/static/js/runtime-main.76160a2a.js"
+  }
+]);
